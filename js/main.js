0b15a4141e6a04d0ea036febb7a9d66a7e32946d
Irin Israel <irinisrael@icloud.com>
2015-11-16 13:50:42 -0500

    made both forms the same

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-	$('button').click(function() {
-		alert('Thank you for stopping plant abuse!');
-	});
+    $('button').click(function() {
+        alert('Thank you for stopping plant abuse!');
+    });
 })
